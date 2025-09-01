15536e541ba81d53b60ab25c19cb7decdc97151c
uday1299 <uday.yadav3477@gmail.com>
2025-09-01 12:04:33 +0530

    user portal completed

--- NO_KITCHEN_APP/models.py
+++ NO_KITCHEN_APP/models.py
@@ -425,29 +425,29 @@ class NoKitchenMenu(models.Model):
 
 
 
-from django.db import models
-from django.contrib.auth.models import User
+# from django.db import models
+# from django.contrib.auth.models import User
 
-class NoKitchenMenu(models.Model):
-    MEAL_CHOICES = [
-        ('breakfast', 'Breakfast'),
-        ('lunch', 'Lunch'),
-        ('dinner', 'Dinner'),
-        ('snack', 'Snacks'),
-        ('dessert', 'Desserts'),
-        ('special', 'Special Package / Event'),
-    ]
-    meal_type = models.CharField(max_length=20, choices=MEAL_CHOICES)
-    item_name = models.CharField(max_length=100)
-    price = models.DecimalField(max_digits=6, decimal_places=2)
-    quantity = models.CharField(max_length=50)
-    image = models.ImageField(upload_to='menu_images/', blank=True, null=True)
-    description = models.TextField(blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    stock = models.PositiveIntegerField(default=100)
+# class NoKitchenMenu(models.Model):
+#     MEAL_CHOICES = [
+#         ('breakfast', 'Breakfast'),
+#         ('lunch', 'Lunch'),
+#         ('dinner', 'Dinner'),
+#         ('snack', 'Snacks'),
+#         ('dessert', 'Desserts'),
+#         ('special', 'Special Package / Event'),
+#     ]
+#     meal_type = models.CharField(max_length=20, choices=MEAL_CHOICES)
+#     item_name = models.CharField(max_length=100)
+#     price = models.DecimalField(max_digits=6, decimal_places=2)
+#     quantity = models.CharField(max_length=50)
+#     image = models.ImageField(upload_to='menu_images/', blank=True, null=True)
+#     description = models.TextField(blank=True)
+#     created_at = models.DateTimeField(auto_now_add=True)
+#     stock = models.PositiveIntegerField(default=100)
 
-    def __str__(self):
-        return f"{self.item_name} ({self.get_meal_type_display()})"
+#     def __str__(self):
+#         return f"{self.item_name} ({self.get_meal_type_display()})"
 
 
 
